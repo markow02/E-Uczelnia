0a9bf29f4e7354f17fb1ab25791729519f01f912
markow02 <114979874+markow02@users.noreply.github.com>
2025-02-14 19:00:36 +0100

    Update ActivityClubServiceTests.cs

--- src/University.Tests/ActivityClubServiceTests.cs
+++ src/University.Tests/ActivityClubServiceTests.cs
@@ -24,7 +24,7 @@ namespace University.Tests
             _mockContext = new Mock<UniversityContext>();
             _mockSet = new Mock<DbSet<ActivityClub>>();
 
-            // Setup the mock DbSet (using async query provider helpers)
+
             var emptyData = new List<ActivityClub>().AsQueryable();
             _mockSet.As<IQueryable<ActivityClub>>().Setup(m => m.Provider)
                 .Returns(new TestAsyncQueryProvider<ActivityClub>(emptyData.Provider));
@@ -35,16 +35,15 @@ namespace University.Tests
             _mockSet.As<IQueryable<ActivityClub>>().Setup(m => m.GetEnumerator())
                 .Returns(emptyData.GetEnumerator());
 
-            // Setup both Set<T>() and the ActivityClubs property
+
             _mockContext.Setup(m => m.Set<ActivityClub>()).Returns(_mockSet.Object);
             _mockContext.Setup(m => m.ActivityClubs).Returns(_mockSet.Object);
 
             _service = new ActivityClubService(_mockContext.Object);
         }
 
-
         [Fact]
-        public async Task LoadDataAsync_ReturnsAllActivityClubs()
+        public async Task LoadDataAsync_ReturnsAllActivityClubs_Positive()
         {
             // Arrange
             var data = new List<ActivityClub>
@@ -68,7 +67,7 @@ namespace University.Tests
         }
 
         [Fact]
-        public async Task LoadDataAsync_ReturnsEmptyList_WhenNoActivityClubs()
+        public async Task LoadDataAsync_ReturnsEmptyList_WhenNoActivityClubs_Negative()
         {
             // Arrange
             var data = new List<ActivityClub>().AsQueryable();
@@ -86,33 +85,7 @@ namespace University.Tests
         }
 
         [Fact]
-        public async Task IsValidAsync_ReturnsFalse_WhenActivityClubNameIsEmpty()
-        {
-            // Arrange
-            var activityClub = new ActivityClub { ActivityClubName = "", MeetingDay = "Monday", ActivityClubDescription = "Description" };
-
-            // Act
-            var result = await _service.IsValidAsync(activityClub);
-
-            // Assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public async Task IsValidAsync_ReturnsFalse_WhenMeetingDayIsEmpty()
-        {
-            // Arrange
-            var activityClub = new ActivityClub { ActivityClubName = "Club", MeetingDay = "", ActivityClubDescription = "Description" };
-
-            // Act
-            var result = await _service.IsValidAsync(activityClub);
-
-            // Assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public async Task IsValidAsync_ReturnsTrue_WhenActivityClubIsValid()
+        public async Task IsValidAsync_ReturnsTrue_WhenActivityClubIsValid_Positive()
         {
             // Arrange
             var activityClub = new ActivityClub { ActivityClubName = "Club", MeetingDay = "Monday", ActivityClubDescription = "Description" };
@@ -125,7 +98,20 @@ namespace University.Tests
         }
 
         [Fact]
-        public async Task SaveDataAsync_AddsNewActivityClub_WhenActivityClubIdIsZero()
+        public async Task IsValidAsync_ReturnsFalse_WhenActivityClubNameIsEmpty_Negative()
+        {
+            // Arrange
+            var activityClub = new ActivityClub { ActivityClubName = "", MeetingDay = "Monday", ActivityClubDescription = "Description" };
+
+            // Act
+            var result = await _service.IsValidAsync(activityClub);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task SaveDataAsync_AddsNewActivityClub_WhenActivityClubIdIsZero_Positive()
         {
             // Arrange
             var activityClub = new ActivityClub { ActivityClubId = 0, ActivityClubName = "Club", MeetingDay = "Monday", ActivityClubDescription = "Description" };
@@ -139,21 +125,21 @@ namespace University.Tests
         }
 
         [Fact]
-        public async Task SaveDataAsync_UpdatesExistingActivityClub_WhenActivityClubIdIsNotZero()
+        public async Task SaveDataAsync_AddsNewActivityClub_WhenActivityClubIdIsZero_Negative()
         {
             // Arrange
-            var activityClub = new ActivityClub { ActivityClubId = 1, ActivityClubName = "Club", MeetingDay = "Monday", ActivityClubDescription = "Description" };
+            var activityClub = new ActivityClub { ActivityClubId = 0, ActivityClubName = "Club", MeetingDay = "Monday", ActivityClubDescription = "Description" };
 
-            // Act
-            await _service.SaveDataAsync(activityClub);
+            _mockSet.Setup(m => m.Add(It.IsAny<ActivityClub>())).Throws(new DbUpdateException());
 
-            // Assert
-            _mockSet.Verify(m => m.Update(It.IsAny<ActivityClub>()), Times.Once);
-            _mockContext.Verify(m => m.SaveChangesAsync(default), Times.Once);
+            // Act & Assert
+            await Assert.ThrowsAsync<DbUpdateException>(() => _service.SaveDataAsync(activityClub));
+            _mockSet.Verify(m => m.Add(It.IsAny<ActivityClub>()), Times.Once);
+            _mockContext.Verify(m => m.SaveChangesAsync(default), Times.Never);
         }
 
         [Fact]
-        public async Task GetActivityClubByIdAsync_ReturnsActivityClub_WhenIdExists()
+        public async Task GetActivityClubByIdAsync_ReturnsActivityClub_WhenIdExists_Positive()
         {
             // Arrange
             var activityClub = new ActivityClub { ActivityClubId = 1, ActivityClubName = "Club", MeetingDay = "Monday", ActivityClubDescription = "Description" };
@@ -174,7 +160,7 @@ namespace University.Tests
         }
 
         [Fact]
-        public async Task GetActivityClubByIdAsync_ReturnsNull_WhenIdDoesNotExist()
+        public async Task GetActivityClubByIdAsync_ReturnsNull_WhenIdDoesNotExist_Negative()
         {
             // Arrange
             var data = new List<ActivityClub>().AsQueryable();
@@ -195,91 +181,89 @@ namespace University.Tests
     }
 }
 
-    // Helper class to support async query provider
-    internal class TestAsyncQueryProvider<TEntity> : IAsyncQueryProvider
+// Helper class to support async query provider
+internal class TestAsyncQueryProvider<TEntity> : IAsyncQueryProvider
+{
+    private readonly IQueryProvider _inner;
+
+    internal TestAsyncQueryProvider(IQueryProvider inner)
     {
-        private readonly IQueryProvider _inner;
-
-        internal TestAsyncQueryProvider(IQueryProvider inner)
-        {
-            _inner = inner;
-        }
-
-        public IQueryable CreateQuery(Expression expression)
-        {
-            return new TestAsyncEnumerable<TEntity>(expression);
-        }
-
-        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
-        {
-            return new TestAsyncEnumerable<TElement>(expression);
-        }
-
-        public object Execute(Expression expression)
-        {
-            return _inner.Execute(expression);
-        }
-
-        public TResult Execute<TResult>(Expression expression)
-        {
-            return _inner.Execute<TResult>(expression);
-        }
-
-        public IAsyncEnumerable<TResult> ExecuteAsync<TResult>(Expression expression)
-        {
-            return new TestAsyncEnumerable<TResult>(expression);
-        }
-
-        public ValueTask<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
-        {
-            return new ValueTask<TResult>(Execute<TResult>(expression));
-        }
-
-        TResult IAsyncQueryProvider.ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
+        _inner = inner;
     }
 
-
-    internal class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    public IQueryable CreateQuery(Expression expression)
     {
-        public TestAsyncEnumerable(IEnumerable<T> enumerable)
-            : base(enumerable)
-        { }
-
-        public TestAsyncEnumerable(Expression expression)
-            : base(expression)
-        { }
-
-        public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
-        {
-            return new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
-        }
-
-        IQueryProvider IQueryable.Provider => new TestAsyncQueryProvider<T>(this);
+        return new TestAsyncEnumerable<TEntity>(expression);
     }
 
-    internal class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+    public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
     {
-        private readonly IEnumerator<T> _inner;
-
-        public TestAsyncEnumerator(IEnumerator<T> inner)
-        {
-            _inner = inner;
-        }
-
-        public ValueTask DisposeAsync()
-        {
-            _inner.Dispose();
-            return ValueTask.CompletedTask;
-        }
-
-        public ValueTask<bool> MoveNextAsync()
-        {
-            return new ValueTask<bool>(_inner.MoveNext());
-        }
-
-        public T Current => _inner.Current;
+        return new TestAsyncEnumerable<TElement>(expression);
     }
 
+    public object Execute(Expression expression)
+    {
+        return _inner.Execute(expression);
+    }
+
+    public TResult Execute<TResult>(Expression expression)
+    {
+        return _inner.Execute<TResult>(expression);
+    }
+
+    public IAsyncEnumerable<TResult> ExecuteAsync<TResult>(Expression expression)
+    {
+        return new TestAsyncEnumerable<TResult>(expression);
+    }
+
+    public ValueTask<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+    {
+        return new ValueTask<TResult>(Execute<TResult>(expression));
+    }
+
+    TResult IAsyncQueryProvider.ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+internal class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+{
+    public TestAsyncEnumerable(IEnumerable<T> enumerable)
+        : base(enumerable)
+    { }
+
+    public TestAsyncEnumerable(Expression expression)
+        : base(expression)
+    { }
+
+    public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+    {
+        return new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+    }
+
+    IQueryProvider IQueryable.Provider => new TestAsyncQueryProvider<T>(this);
+}
+
+internal class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+{
+    private readonly IEnumerator<T> _inner;
+
+    public TestAsyncEnumerator(IEnumerator<T> inner)
+    {
+        _inner = inner;
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        _inner.Dispose();
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask<bool> MoveNextAsync()
+    {
+        return new ValueTask<bool>(_inner.MoveNext());
+    }
+
+    public T Current => _inner.Current;
+}
